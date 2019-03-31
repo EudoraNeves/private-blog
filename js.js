28c673c19cb5b53f4d9aea0d4e528ac71528fe63
Eudora <1052288068@qq.com>
2019-03-31 20:37:45 +0800

    31/3

--- js.js
+++ js.js
@@ -1,5 +1,5 @@
 
-var greetings = document.getElementById("greetings");
+/*var greetings = document.getElementById("greetings");
 var time = new Date();
 var h = time.getHours();
 if (h >= 19 && h <= 21) {
@@ -14,8 +14,82 @@ else {
 
 function alertQQ() {
     alert("Add me in QQ: 1052288068");
-}
+}*/
 
 //document.getElementsByClassName('qq').addEventListener('click', alertQQ()); <-- this or:
 
 //document.getElementsByClassName('qq').addEventListener('click', (() => { alert('add me in qq: xxxxxxxxx') }) )
+
+$(document).ready(function () {
+    //greetings
+    var h = new Date().getHours();
+    if (h >= 19 && h <= 21) {
+        $("#greetings").text("Good evening");
+    }
+    if (h > 9 && h <= 12) {
+        $("#greetings").text("Good morning");
+    }
+    else {
+        $("#greetings").text("Good afternoon");
+    }
+
+
+    //qq
+    $(".qq").on('click', function () {
+        alert("Add me in qq: 1052288068")
+    })
+
+
+    //banners
+
+
+
+
+
+    //About me 
+    //comment section
+
+    $("#add").on("click", function () {
+        if (val !== "") {
+            var val = $("input").val();
+            var list = $("<li></li>").text(val);
+            list.append("<span class='cancel'>X</span>");
+            $("#commentlists").append(list);
+            $("#commentarea").val("");
+
+            var i;
+            for (i = 0; i < cancelArray.length; i++) {
+
+                cancel.on("click", function () {
+                    var cancel = $(".cancel").eq[i];
+                    alert("You sure you want to delete it?");
+                    $(this).parent().remove();
+
+                })
+            }
+        }
+
+        })
+
+        $(".head-nav-item").on("mouseover", function(){
+            $(".submeanu").slideToggle(1000);
+    
+        })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
+
